613db41ba6494bf8fef38744f85d129633823907
danielDesarrollo <danielcastrillon0528@gmail.com>
2023-05-11 22:03:44 -0500

    fit: cambios

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -19,7 +19,6 @@ const Formulario = ({pacientes, setPacientes, paciente}) => {
   const generarId = () => {
     const random = Math.random().toString(36).substring(2);
     const fecha = Date.now().toString(36);
-    
     return random+fecha;
   }
 
