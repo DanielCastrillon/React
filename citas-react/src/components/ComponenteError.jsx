e69cae18e704760c2c0b76e6d2c7a5a7807ee0c8
danielDesarrollo <danielcastrillon0528@gmail.com>
2023-05-17 22:14:27 -0500

    fit: aaa

--- citas-react/src/components/ComponenteError.jsx
+++ citas-react/src/components/ComponenteError.jsx
@@ -4,7 +4,7 @@ function Error({children}) {
   return (
     <div>
         <div className="bg-red-800 text-white text-center p-2 uppercase font-bold mb-3 rounded-md">
-          {children}
+          {children} 
         </div>
     </div>
   )
